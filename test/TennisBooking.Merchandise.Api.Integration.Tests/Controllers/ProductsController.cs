46a65201b933ffc64702cbcd9b8d538f067bb67a
Jaroslaw Paradysz <jaroslaw.a.paradysz@capgemini.com>
2021-10-13 07:25:15 +0200

    Added tests for ProductsController

---
+++ test/TennisBooking.Merchandise.Api.Integration.Tests/Controllers/ProductsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using TennisBooking.Merchandise.Api.Integration.Tests.Fakes;
+using TennisBookings.Merchandise.Api;
+using TennisBookings.Merchandise.Api.Data;
+using Xunit;
+
+namespace TennisBooking.Merchandise.Api.Integration.Tests.Controllers
+{
+    public class ProductsController : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly WebApplicationFactory<Startup> _factory;
+
+        public ProductsController(WebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task GetAll_ReturnsSuccessStatusCode()
+        {
+            var fakeProductRepository = new FakeProductDataRepository();
+
+            HttpClient client = _factory.WithWebHostBuilder(builder => builder.ConfigureTestServices(
+                services => services.AddTransient<IProductDataRepository>(sys => fakeProductRepository)
+            ))
+            .CreateClient();
+
+            HttpResponseMessage response = await client.GetAsync($"http://localhost/api/products/");
+            response.EnsureSuccessStatusCode();
+
+            Assert.Equal("application/json", response.Content.Headers.ContentType.MediaType);
+        }
+    }
+}
---
+++ test/TennisBooking.Merchandise.Api.Integration.Tests/Fakes/FakeProductDataRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TennisBookings.Merchandise.Api.Data;
+using TennisBookings.Merchandise.Api.DomainModels;
+
+namespace TennisBooking.Merchandise.Api.Integration.Tests.Fakes
+{
+    public class FakeProductDataRepository : IProductDataRepository
+    {
+        public Task<AddProductResult> AddProductAsync(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> GetByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IReadOnlyCollection<Product>> GetProductsAsync()
+        {
+            await Task.CompletedTask;
+            return Enumerable.Empty<Product>().ToList();
+        }
+
+        public Task<IReadOnlyCollection<Product>> GetProductsForCategoryAsync(string category)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
